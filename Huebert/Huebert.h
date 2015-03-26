6d364229675db270881474574285735e5246c0c2
Zak Auerbach <zak@zaksoup.com>
2015-03-26 13:57:34 -0700

    Initial Commit

---
+++ Huebert/Huebert.h
@@ -0,0 +1,19 @@
+//
+//  Huebert.h
+//  Huebert
+//
+//  Created by Zachary Auerbach on 3/26/15.
+//  Copyright (c) 2015 Zachary Auerbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Huebert.
+FOUNDATION_EXPORT double HuebertVersionNumber;
+
+//! Project version string for Huebert.
+FOUNDATION_EXPORT const unsigned char HuebertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Huebert/PublicHeader.h>
+
+
